2a24a7ff382a3398c61311ca0ef0f7331ec454a0
MountainGod2 <MountainGod2@users.noreply.github.com>
2022-09-26 03:39:41 +0000

    deploy: dfff1dc6ce253082a0eb1cd5efadb8597a649623

--- css/addons/unraid/login-page/ibracorp/js/glitch_head.js
+++ css/addons/unraid/login-page/ibracorp/js/glitch_head.js
@@ -2,6 +2,6 @@
 // Source: https://www.youtube.com/
 
 let video = `
-<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/mountaingod2.github.io\/css\/addons\/unraid\/login-page\/ibracorp\/video\/glitch_head.mp4" type="video\/mp4">Video not supported</video></div>
+<div class="background-wrap"><video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted"> <source src="https:\/\/mountaingod2.github.io\/theme.park\/css\/addons\/unraid\/login-page\/ibracorp\/video\/glitch_head.mp4" type="video\/mp4">Video not supported</video></div>
 `
 document.body.innerHTML += video
